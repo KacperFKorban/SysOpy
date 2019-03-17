5ef4c80a467a290dcd548f0c8c3953b8ec69c55b
KacperFKorban <kacper.f.korban@gmail.com>
2019-03-17 21:04:46 +0100

    Chyba dziala zad2a

--- Lab2/zad2/main.c
+++ Lab2/zad2/main.c
@@ -9,68 +9,77 @@
 #include <unistd.h>
 #include <string.h>
 #include <dirent.h>
+#include <limits.h>
 
 char* formatdate(char* str, time_t val) {
         strftime(str, 36, "%d.%m.%Y %H:%M:%S", localtime(&val));
         return str;
 }
 
-void traverse(char* path, char* sign, time_t mtime) {
+char* file_type(int st_mode) {
+    if(S_ISREG(st_mode)) {
+        return "file";
+    } else if(S_ISDIR(st_mode)) {
+        return "dir";
+    } else if(S_ISCHR(st_mode)) {
+        return "char dev";
+    } else if(S_ISBLK(st_mode)) {
+        return "block dev";
+    } else if(S_ISFIFO(st_mode)) {
+        return "fifo";
+    } else if(S_ISLNK(st_mode)) {
+        return "slink";
+    } else if(S_ISSOCK(st_mode)) {
+        return "sock";
+    }
+    return "unknown";
+}
+
+void traverse(char* path, char* comp, time_t mtime) {
     DIR* root_of_search = opendir(path);
+    char new_path[PATH_MAX + 1];
 
     if(root_of_search == NULL) return;
 
     struct dirent* de;
-    struct stat* st = (struct stat*) calloc(1, sizeof(struct stat));
+    struct stat st;
 
     char str[36];
 
-    char c = sign[0];
+    char c = comp[0];
     while((de = readdir(root_of_search)) != NULL) {
         int f = 0;
-        char* new_path = strcat(path, de->d_name);
-        stat(new_path, st);
+        if(strcmp(de->d_name, ".") == 0 || strcmp(de->d_name, "..") == 0)
+            continue;
+        realpath(de->d_name, new_path);
+        lstat(new_path, &st);
         switch(c) {
             case '<':
-                if(st->st_mtime < mtime) {
+                if(st.st_mtime < mtime) {
                     f = 1;
                 }
+                break;
             case '=':
-                if(st->st_mtime == mtime) {
+                if(st.st_mtime == mtime) {
                     f = 1;
                 }
+                break;
             case '>':
-                if(st->st_mtime > mtime) {
+                if(st.st_mtime > mtime) {
                     f = 1;
                 }
+                break;
         }
-        
-        if(st->st_mode & S_IFDIR) {
-            traverse(new_path, sign, mtime);
+
+        if(st.st_mode & S_IFDIR) {
+            traverse(new_path, comp, mtime);
         }
         if(f == 1) {
-            char* file_type;
-            if(st->st_mode & S_IFREG) {
-                file_type = "file";
-            } else if(st->st_mode & S_IFDIR) {
-                file_type = "dir";
-            } else if(st->st_mode & S_IFCHR) {
-                file_type = "char dev";
-            } else if(st->st_mode & S_IFBLK) {
-                file_type = "block dev";
-            } else if(st->st_mode & S_IFIFO) {
-                file_type = "fifo";
-            } else if(st->st_mode & S_IFLNK) {
-                file_type = "slink";
-            } else if(st->st_mode & /* TODO */) {
-                file_type = "sock";
-            }
-            printf("%s %s %d %s %s\n", new_path, file_type, st->st_size,
-                formatdate(str, st->st_atime), formatdate(str, st->st_mtime));
+            printf("%s %s %ld %s %s\n", new_path, file_type(st.st_mode), st.st_size,
+                formatdate(str, st.st_atime), formatdate(str, st.st_mtime));
         }
     }
 
-free(st);
     closedir(root_of_search);
 }
 
@@ -78,13 +87,15 @@ int main(int argc, char* argv[]) {
 
     if(argc != 4) return -1;
     char* path = argv[1];
-    char* sign = argv[2];
+    char* comp = argv[2];
     char* date = argv[3];
 
     char* ptr;
-    time_t mtime = strtol(data, ptr, 10);
+    time_t mtime = strtol(date, &ptr, 10);
 
-    traverse(path, sign, mtime);
+    // TODO sciezka wzgledna i wyswietlanie bezwzglednej
+
+    traverse(path, comp, mtime);
 
     return 0;
 }
